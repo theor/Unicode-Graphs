2b9f10d3210db3e0c6a8cdee297c43cb0fd44828
Theo Richart <theor@unity3d.com>
2021-03-03 01:54:37 -0500

    smaller serialization

--- src/Serialization.fs
+++ src/Serialization.fs
@@ -1,6 +1,7 @@
 module App.Serialization
 
 open Fable.Core
+open Thoth.Json
 open Types
 open Graph
 
@@ -26,11 +27,20 @@ type SerializationModel = {
             options = m.options
             nodes = m.graph.nodes |> Map.toSeq |> Seq.map snd |> Seq.toArray
             edges = m.graph.edges |> Map.toSeq |> Seq.map snd |> Seq.toArray
-        } 
+        }
+
+let encodeId (id:Id) = Encode.uint32 id.Value
+let decodeId (path:string) (value:obj) = Decode.uint32 path value |> Result.map Id.Id
+let extras = Extra.empty
+             |> Extra.withCustom encodeId decodeId
+let encoder = Thoth.Json.Encode.Auto.generateEncoderCached<SerializationModel>(skipNullField=true, extra=extras)
+let decoder = Thoth.Json.Decode.Auto.generateDecoderCached<SerializationModel>(extra=extras)
 
 let toJson (m:Model): string =
-    Thoth.Json.Encode.Auto.toString(0, SerializationModel.fromModel m)
+    SerializationModel.fromModel m
+    |> encoder
+    |> Thoth.Json.Encode.toString 0
 
 let fromJson(s:string): Result<Model,string> =
-    Thoth.Json.Decode.Auto.fromString(s)
+    Decode.fromString decoder s
     |> Result.map SerializationModel.toModel
